ba063a1bfec57ce5c749ea2ce47bda10d5cfcb41
GiordanoCasale <giordanocasale8@gmail.com>
2025-04-17 14:33:38 +0200

    [UPD] initialize express app and set port number

---
+++ app.js
@@ -0,0 +1,9 @@
+//importazione di express
+const express = require("express")
+
+//inizializzo express dentro la variabile app
+const app = express();
+
+//definizione del numero di porta
+const port = 3000;
+
