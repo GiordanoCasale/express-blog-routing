130141c5513a3bc843a2f6c2dec545ae18718b35
GiordanoCasale <giordanocasale8@gmail.com>
2025-04-17 14:36:09 +0200

    [UPD] define base route and start server in app.js

--- app.js
+++ app.js
@@ -10,3 +10,12 @@ const port = 3000;
 //impostazione dello strumento per recuperare gli asset statici
 app.use(express.static("public"));
 
+//definizione della rotta base
+app.get("/", (req, res) => {
+    res.send("Benvenuti nel mio blog");
+})
+
+//inserimento del metodo che lascia in ascolto il nostro server
+app.listen(port, () => {
+    console.log(`Server del blog in ascolto alla porta ${port}`);
+})
